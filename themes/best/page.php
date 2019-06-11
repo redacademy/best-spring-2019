fc13228403aae1d56e48a62be9bc1090f75483ce
Jeff <basilofbakerstreet@gmail.com>
2019-06-11 15:15:05 -0700

    Update new pages

--- themes/best/page.php
+++ themes/best/page.php
@@ -7,17 +7,17 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php get_template_part('template-parts/content', 'page'); ?>
 
-			<?php endwhile; // End of the loop. ?>
+		<?php endwhile; 
+	?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
