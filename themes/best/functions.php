22a31a3d244a0076e753ff408b282826382388e3
Rishab <rishabagrawal@hotmail.com>
2019-06-12 14:40:19 -0700

    add footer and style little for mobile

--- themes/best/functions.php
+++ themes/best/functions.php
@@ -85,6 +85,8 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  */
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
+	// font awesome//
+	wp_enqueue_style('inhabitent-fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css');
 
 	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
