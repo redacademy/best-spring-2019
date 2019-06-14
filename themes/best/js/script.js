67d60c8a2e8b0730ca729c181f61c09c4e54e3c8
Vincent Wang <a36168120@gmail.com>
2019-06-14 10:15:20 -0700

    quick commit before group pull

--- themes/best/js/script.js
+++ themes/best/js/script.js
@@ -19,28 +19,30 @@
     });
 
     // Hamburger Menu
-    const $hamburger = $('.hamburger-menu');
-    $hamburger.on('click', function(event) {
+    const hamburger = $('.hamburger-menu');
+      hamburger.on('click', function(event) {
       event.preventDefault();
-      $hamburger.toggleClass('is-active');
+      hamburger.toggleClass('is-active');
     });
     
     // Menu Slider
-    const $headerSlider = $('.main-navigation');
-    const $headerNav = $('.header-primary-menu');
-    $headerSlider.on('click', function(event) {
+    const headerSlider = $('.main-navigation');
+    const headerNav = $('.header-primary-menu');
+      headerSlider.on('click', function(event) {
       event.preventDefault();
-      $headerSlider.toggleClass('.open');
-      if ($headerSlider.toggleClass('open')) {
+      headerSlider.toggleClass('.open');
+      if (headerSlider.toggleClass('open')) {
         event.preventDefault();
-        $headerNav.show();
+        headerNav.show();
       } 
     });
 
     //Menu Button
-    let $dropDown = bestCustomScripts.theme_directory + 'Assets/vector-icons/icons/chevrons/down.svg'
-    $('.menu-item-204').append($dropDown);
-    $('.menu-item-261').append($dropDown)
+    let php = '<?php echo get_template_directory_uri(); ?>';
+    // let dropDown = bestCustomScripts.theme_directory + 'Assets/vector-icons/icons/chevrons/down.svg'
+    let dropDown = '<img src="' + php + 'Assets/vector-icons/icons/chevrons/down.svg">'
+    $('.menu-item-204').append(dropDown);
+    $('.menu-item-261').append(dropDown)
 
     // Flickity
     $('.main-carousel').flickity({
