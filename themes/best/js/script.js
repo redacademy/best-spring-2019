c694adfac4e7f9a604494f906e2b77ffa8e7e39f
Jeff <basilofbakerstreet@gmail.com>
2019-06-14 09:14:26 -0700

    Tab content code added

--- themes/best/js/script.js
+++ themes/best/js/script.js
@@ -1,18 +1,30 @@
-(function ($) {
-    $(function() {
+(function($) {
+  $(function() {
+    const searchBar = $('.search-field');
 
-        const searchBar = $('.search-field');
-      
-        searchBar.hide();
-        $('.search-submit').on('click', function(event) {
-          event.preventDefault();
-          searchBar.toggle('show');
-          searchBar.focus();
-        });
-        $('.main-carousel').flickity({
-          // options
-          cellAlign: 'left',
-          contain: true
-        });
+    searchBar.hide();
+    $('.search-submit').on('click', function(event) {
+      event.preventDefault();
+      searchBar.toggle('show');
+      searchBar.focus();
     });
+    $('.main-carousel').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
+
+    //START OF TAB MENU CODE
+    const $tabLinks = $('.tab-link');
+    const $tabContent = $('.tab-content');
+
+    $tabLinks.on('click', function(event) {
+      event.preventDefault();
+
+      let tabIndex = $tabLinks.index(this);
+      $tabContent.hide();
+      $tabContent.eq(tabIndex).show();
+    });
+    //END OF TAB MENU CODE
+  });
 })(jQuery);
