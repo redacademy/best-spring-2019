d425a1e6272b6024c7f143ed2e504adb1e8c9555
Tenzin <yaungchen.tenzin@gmail.com>
2019-06-14 15:23:30 -0700

    edit footer php and scss, add trankslink logo and style mobile footer

--- themes/best-bc/footer.php
+++ themes/best-bc/footer.php
@@ -9,30 +9,35 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="footer-blocks container"> 
-				<div class="footer-block-item">
-					<div class="best-footer-logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/vector-icons/best-logo/Best-v1-black.svg" alt="footer best logo">
-					</div> <!--footer logo ending-->
-				</div> <!--block item ending-->	
-				<div class="footer-block-item">
-					<div class="social-media">
-						<h3 class="follow-text">Follow us</h3>
-						<div>
-						<span><i class="fab fa-facebook"></i></span>
-						<span><i class="fab fa-instagram"></i></span>
-						<span><i class="fab fa-twitter-square"></i></span>
+				<div class="footer-content">
+				
+					<div class="best-footer-logo">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/vector-icons/best-logo/Best-v1-black.svg" alt="footer-best-logo">
+						<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
+						<!--end of footer logo-->
+					</div> <!--block item ending-->	
+				
+					<div class=social-media">
+						<p class="follow-text">Follow us</p>
+						 <div class ="social-media-icons">
+							<span><i class="fab fa-facebook"></i></span>
+							<span><i class="fab fa-instagram"></i></span>
+							<span><i class="fab fa-twitter-square"></i></span>
 						</div>
-
+					</div>
+				
+				</div>	
+				<h2 class="major-sponsor-text">Our Major Sponsor:</h2>
+				<img class="translink-logo-grey" src="<?php echo get_template_directory_uri();
+				 ?>/assets/images/translink-logo-grey.png" alt="translink-logo"">	
 						
-					</div> <!--social media ending-->
-				</div> <!-- footer block item second ending-->
-			</div> <!--footer blocks container ending-->
+			
+			
+			
 
-			<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
+			
 
-				<div class="site-info">
-					
-				</div><!-- .site-info -->
+				<!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
