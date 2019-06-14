b49fc5effc9b177e98f8737beb67009f4e3d8966
Vincent Wang <a36168120@gmail.com>
2019-06-14 10:49:01 -0700

    add missing codes

--- themes/best-bc/js/script.js
+++ themes/best-bc/js/script.js
@@ -1,7 +1,10 @@
 (function($) {
   $(function() {
-    const searchBar = $('.search-field');
 
+    // Start of Header //
+
+    // Search Bar
+    const searchBar = $('.search-field');
     searchBar.hide();
     $('.search-submit').on('click', function(event) {
       event.preventDefault();
@@ -10,23 +13,46 @@
     });
 
     searchBar.on('blur', function(event) {
-      event.preventDefault();
-      if (searchBar.val() === '') {
+    event.preventDefault();
+      if ( searchBar.val() === '' ) {
         searchBar.hide('slide');
       }
     });
+    
+    // Hamburger Menu
+    const hamburger = $('.hamburger-menu');
+      hamburger.on('click', function(event) {
+      event.preventDefault();
+      hamburger.toggleClass('is-active');
+    });
+    
+    // Menu Slider
+    const headerSlider = $('.main-navigation');
+    const headerNav = $('.header-primary-menu');
+      headerSlider.on('click', function(event) {
+      event.preventDefault();
+      headerSlider.toggleClass('.open');
+      if (headerSlider.toggleClass('open')) {
+        event.preventDefault();
+        headerNav.show();
+      } 
+    });
 
+    //Menu Button
+    let php = '<?php echo get_template_directory_uri(); ?>';
+    // let dropDown = bestCustomScripts.theme_directory + 'Assets/vector-icons/icons/chevrons/down.svg'
+    let dropDown = '<img src="' + php + 'Assets/vector-icons/icons/chevrons/down.svg">'
+    $('.menu-item-204').append(dropDown);
+    $('.menu-item-261').append(dropDown);
+    // End of Header //
+    
+    // Start of Flickity //
     $('.main-carousel').flickity({
       // options
       cellAlign: 'left',
       contain: true
     });
-
-    $('.main-carousel').flickity({
-      // options
-      cellAlign: 'left',
-      contain: true
-    });
+    // End of Flickity //
 
     //START OF TAB MENU CODE
     const $tabLinks = $('.tab-link');
