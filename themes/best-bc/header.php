525f117bc6eb2d70a5854e9ffa99a126329229c1
Vincent Wang <a36168120@gmail.com>
2019-06-14 11:04:03 -0700

    remove flickity and commented out css links

--- themes/best-bc/header.php
+++ themes/best-bc/header.php
@@ -14,10 +14,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<link rel="stylesheet" href="/path/to/flickity.css" media="screen">
-	<!-- <link href="dist/hamburgers.css" rel="stylesheet"> -->
 	
-
 	<?php wp_head(); ?>
 </head>
 
