7eea34ff48d1a65df9b69715dacf6edb33b65e80
Rishab <rishabagrawal@hotmail.com>
2019-06-14 10:15:42 -0700

    add front-page scss, add styling for grid display for involved section

--- themes/best/front-page.php
+++ themes/best/front-page.php
@@ -9,13 +9,23 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
+
         <?php while (have_posts()) : the_post(); ?>
 
             <?php get_template_part('template-parts/content', 'page'); ?>
 
         <?php endwhile;
     ?>
-
+      <div class="involved-container">
+        <div class="donate-and-member-block">
+            <div class="donate"></div>
+            <div class="membership"></div>
+        </div><!--donateandmeber-block-->
+        <div class="sponsor-and-volunteer-block">
+            <div class="sponsor"></div>
+            <div calss="volunteer"></div>
+        </div><!--sponsorandvolunteer-block-->
+      </div> <!--involved container-->
     </main><!-- #main -->
 </div><!-- #primary -->
 
