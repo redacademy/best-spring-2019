595563cc0807a6fb011f9add095ff00cdc58ec51
Rishab <rishabagrawal@hotmail.com>
2019-06-17 10:57:21 -0700

    style involved section mobile and desktop

--- themes/best-bc/front-page.php
+++ themes/best-bc/front-page.php
@@ -21,19 +21,10 @@ get_header(); ?>
             <?php get_template_part('template-parts/content', 'page'); ?>
             <div class="main-carousel">
   <div class="carousel-cell">
-<<<<<<< HEAD
 
   <img src="<?php echo get_template_directory_uri(); ?>/assets/Images/city-logo.png" alt="city-logo"/>
   </div>
   <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/Images/icbc-logo.png" alt="icbc-logo"/>
-=======
-  Hey How are you?
-  <img src="<?php echo get_template_directory_uri(); ?>assets/images/modo-logo.png" alt="flickity"/>
-  </div>
-  <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>assets/images/modo-logo.png" alt="flickity"/>
-  </div>
-  <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>assets/images/modo-logo.png" alt="flickity"/>
->>>>>>> 7276ad850f14e80b379eab3c282437a201ed4d8f
   </div>
   <!-- <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>assets/Images/modo-logo.png" alt="flickity"/>
   </div> -->
