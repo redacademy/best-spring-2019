ea00ac2602f7283107c3f5c212557f2cf7b75625
Tenzin <yaungchen.tenzin@gmail.com>
2019-08-07 12:27:58 -0700

    change footer best logo

--- themes/best-bc/header.php
+++ themes/best-bc/header.php
@@ -46,7 +46,7 @@
 
 			<div class="best-header-logo">
 				<a href="<?php echo get_option("siteurl"); ?>">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/vector-icons/best-logo/best-newlogo.png" alt="">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/vector-icons/best-logo/best-newlogolrg.png" alt="">
 				</a>
 			</div><!-- heder-logo -->
 
